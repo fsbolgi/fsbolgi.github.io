8bf4b48c935a7ac81c6c6b7465e7fe9c1778895f
fsbolgi <francesca.sbolgi@gmail.com>
2018-05-12 15:27:26 +0200

    api works?

--- Sciviz/histogram.js
+++ Sciviz/histogram.js
@@ -6,14 +6,30 @@ var height = 550;
 var svg_histoA = d3.select(".histoA_svg");
 var svg_histoB = d3.select(".histoB_svg");
 
-d3.csv("Data/Male/1982-ITALIA.csv", type, function (error, data) {
+//from api: data set from 1991-2001 at:
+// http://apistat.istat.it/?q=getdatajsonnuts&dataset=DCIS_RICPOPRES1991&dim=1,1,1,1,1559&lang=0
+/*
+    dataset: DCIS_RICPOPRES2001 : popolazione residente ricostruita - Anni 1991-2001
+    dataset: DCIS_RICPOPRES1991 : popolazione residente ricostruita - Anni 1982-1991
+    dim: 1) 1 campo territorio, 1 è Cod Italia
+         2) 1 tipo di indicatore demografico, 1 è Cod per popolazione al 1^ gennaio
+         3) 1 età, 1 è il Cod per zero anni
+         4) 1 sesso, 1 è Cod per maschi
+         5) 1559 seleziona periodo, 1559 è il codice per 1982
+    */
+
+d3.json("http://apistat.istat.it/?q=getdatajsonnuts&dataset=DCIS_POPRES1&dim=1,1,0,182,0,0&lang=0", function(e, d){
+    console.log("api try");
+    console.log(d);
+})
+d3.csv("Data/Male/ITALIA.csv", type, function (error, data) {
     console.log(data);
     var max = d3.max(data, function (d) {
         return d.value;
     });
     var xScale = d3.scale.linear()
         .domain([0, max])
-        .range([0, width/2-30]);
+        .range([0, width / 2 - 30]);
 
     var bar_height = (height - 50) / data.length;
 
@@ -77,6 +93,6 @@ d3.csv("Data/Male/1982-ITALIA.csv", type, function (error, data) {
 });
 
 function type(d) {
-    d.value = +d.IT;
+    d.value = +d.A_1982;
     return d;
 }
